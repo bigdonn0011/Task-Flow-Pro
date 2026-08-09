defa2e4209cbdb545bb96f2b573f708d6035481c
bigdonn0011 <bigdonn0011@gmail.com>
2026-08-09 10:05:12 +0100

    Ui fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 
 import { Buttons } from "../component/Buttons";
 import { Form } from "../component/Form";
